0880d3485cc1078c094f562161cff4c40860b037
Nyghtwel <nyghtwel@gmail.com>
2022-02-13 20:23:10 -0500

    Initial Commit

---
+++ FlipgridInterview/FlipgridInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipgridInterviewApp.swift
+//  FlipgridInterview
+//
+//  Created by Nyghtwel on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct FlipgridInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
